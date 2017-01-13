2b3d28b309d6442c88ea6290f4c0c998b7f8b867
raspi0124 <adores0124@yahoo.co.jp>
2017-01-13 14:58:24 +0100

    Update led2.php

--- led2.php
+++ led2.php
@@ -1,21 +1,6 @@
 // this programme for when someone pass side of raspberry pi, led will light up.
-// this programme is not finish.
-
-<? php
-resource fam_open ([ string $appname ] );
-
-//Check is new image are on image folder? ( image will capture when usb camera thought that there is moving.
-
-resource fam_monitor_directory ( resource $fam , string images )
-	if ($fam = FAMCreated) {
-        	file_put_contents('/sys/class/gpio/export', 25);
-		file_put_contents('/sys/class/gpio/gpio25/direction', 'out');
-		file_put_contents('/sys/class/gpio/gpio25/value', 1);
-		echo "Led has to be lighting up now!";
-	}elseif($fam = False){
-    		echo "Nope,There is no moving.";
-	}else {
-    		echo "something wrong happen.it's might be error.";
-}
-
-	?>
+<?php
+file_put_contents('/sys/class/gpio/export', 25);
+file_put_contents('/sys/class/gpio/gpio25/direction', 'out');
+file_put_contents('/sys/class/gpio/gpio25/value', 1);
+?>
